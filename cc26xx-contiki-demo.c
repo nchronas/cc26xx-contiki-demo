c0c4e93531e568d98b41a52142d158df1594a1c5
nchronas <nchronas@gmail.com>
2016-01-25 13:35:10 +0200

    adding finctionality and files

---
+++ cc26xx-contiki-demo.c
@@ -0,0 +1,26 @@
+#include "contiki.h"
+#include "ti-lib.h"
+
+#include <stdio.h>
+#include <stdint.h>
+
+PROCESS(cc26xx_contiki_demo_process, "cc26xx contiki demo process");
+AUTOSTART_PROCESSES(&cc26xx_contiki_demo_process);
+
+ROCESS_THREAD(cc26xx_contiki_demo_process, ev, data)
+{
+
+	PROCESS_BEGIN();
+
+	printf("Hello Contiki\nCC26XX contiki demo\n");
+
+	while(1) {
+
+		PROCESS_YIELD();
+
+		printf("From the loop\n");
+    
+	}
+
+	PROCESS_END();
+}
