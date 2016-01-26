b48a5e57165dfc53d6b249edf386f25ec547f60c
nchronas <nchronas@gmail.com>
2016-01-26 23:33:02 +0200

    setting pull up in gpio

--- cc26xx-contiki-demo.c
+++ cc26xx-contiki-demo.c
@@ -131,12 +131,15 @@ PROCESS_THREAD(ledpack_process, ev, data)
 	while(1) {
 
 		ti_lib_rom_ioc_pin_type_gpio_output(DEV_LED_WHITE);
+		ti_lib_ioc_io_port_pull_set(DEV_LED_WHITE, IOC_IOPULL_UP);
 		ti_lib_gpio_pin_write(DEV_LED_WHITE, 1);
 
 		ti_lib_rom_ioc_pin_type_gpio_output(DEV_LED_RED);
+		ti_lib_ioc_io_port_pull_set(DEV_LED_RED, IOC_IOPULL_UP);
 		ti_lib_gpio_pin_write(DEV_LED_RED, 0);
 
 		ti_lib_rom_ioc_pin_type_gpio_output(DEV_LED_GREEN);
+		ti_lib_ioc_io_port_pull_set(DEV_LED_RED, IOC_IOPULL_UP);
 		ti_lib_gpio_pin_write(DEV_LED_GREEN, 1);
 
 		PROCESS_YIELD();
