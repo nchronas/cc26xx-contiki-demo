5ab3051ef6531a7e1dc484b89becd4454b5624ad
nchronas <nchronas@gmail.com>
2016-01-26 11:44:42 +0200

    blink a led

--- cc26xx-contiki-demo.c
+++ cc26xx-contiki-demo.c
@@ -1,4 +1,5 @@
 #include "contiki.h"
+#include "dev/leds.h"
 #include "ti-lib.h"
 
 #include <stdio.h>
@@ -8,36 +9,35 @@
 
 static struct etimer et;
 
-static int counterA = 0 ;
-int counterB = 0 ;
 
 PROCESS(cc26xx_contiki_demo_process, "cc26xx contiki demo process");
 AUTOSTART_PROCESSES(&cc26xx_contiki_demo_process);
 
 PROCESS_THREAD(cc26xx_contiki_demo_process, ev, data)
 {
-	int counterC = 0 ;
 
 	PROCESS_BEGIN();
+
+	leds_on(LEDS_RED);
+	leds_off(LEDS_YELLOW);
 
 	printf("Hello Contiki!!!\nCC26XX contiki demo\n");
 
 	etimer_set(&et, CC26XX_DEMO_LOOP_INTERVAL);
 
 	while(1) {
-		int counterD = 0 ;
-		static int counterE = 0 ;
+		static int counterA = 0 ;
 
 		PROCESS_YIELD();
 
 		if(ev == PROCESS_EVENT_TIMER) {
 			if(data == &et) {
 				counterA++;
-				counterB++;
-				counterC++;
-				counterD++;
-				counterE++;
-				printf("5 secs passed, counters: %d %d %d %d %d\n", counterA, counterB, counterC, counterD, counterE);
+
+				leds_toggle(LEDS_YELLOW);
+				leds_toggle(LEDS_RED);
+
+				printf("5 secs passed, time elapsed: %dSEC\n", counterA*5);
 				etimer_set(&et, CC26XX_DEMO_LOOP_INTERVAL);
 			}
 		}
