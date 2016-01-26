96ea48a81cda8a449befbe16111d9451853bd9ae
nchronas <nchronas@gmail.com>
2016-01-26 14:46:07 +0200

    adding a process

--- cc26xx-contiki-demo.c
+++ cc26xx-contiki-demo.c
@@ -11,10 +11,13 @@
 #define CC26XX_DEMO_LOOP_INTERVAL       (CLOCK_SECOND * 5)
 
 static struct etimer et;
+static int counterA = 0 ;
 
+PROCESS(button_process, "button process");
 
 PROCESS(cc26xx_contiki_demo_process, "cc26xx contiki demo process");
 AUTOSTART_PROCESSES(&cc26xx_contiki_demo_process);
+
 
 PROCESS_THREAD(cc26xx_contiki_demo_process, ev, data)
 {
@@ -23,19 +26,20 @@ PROCESS_THREAD(cc26xx_contiki_demo_process, ev, data)
 
 	printf("Hello Contiki!!!\nCC26XX contiki demo\n");
 
-    buzzer_start(1000);
-
 	etimer_set(&et, CC26XX_DEMO_LOOP_INTERVAL);
 
 	while(1) {
-		static int counterA = 0 ;
 
 		if(counterA == 0) {
 			leds_off(LEDS_RED);
 			leds_on(LEDS_GREEN);
+
+		} else if(counterA == 1) {
+			printf("Starting button process\n");
+			AUTOSTART_PROCESSES(&button_process);
 		}
 
-		PROCESS_YIELD();
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et);
 
 		if(ev == PROCESS_EVENT_TIMER) {
 			if(data == &et) {
@@ -46,16 +50,35 @@ PROCESS_THREAD(cc26xx_contiki_demo_process, ev, data)
 				printf("5 secs passed, time elapsed: %dSEC\n", counterA*5);
 				etimer_set(&et, CC26XX_DEMO_LOOP_INTERVAL);
 			}
-		} else if(ev == sensors_event && data == &button_left_sensor ) {
+		} 
+	}
+
+	PROCESS_END();
+
+}
+
+
+PROCESS_THREAD(button_process, ev, data)
+{
+
+	PROCESS_BEGIN();
+
+	printf("Hello from button process\n");
+
+	while(1) {
+
+		PROCESS_YIELD();
+
+		if(ev == sensors_event && data == &button_left_sensor ) {
 			printf("Left button pushed\n");
 			watchdog_reboot();
 		} else if(ev == sensors_event && data ==  &button_right_sensor) {
-			printf("Right button pushed\n");
+			printf("Right button pushed, time: %d\n", counterA*5);
 			if(buzzer_state()) {
 				buzzer_stop();
 			} else {
 				buzzer_start(1000);
-	        }
+			}
 		}
 
 	}
